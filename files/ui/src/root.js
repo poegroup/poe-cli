315a3ecb87b221fe43785e66534dc22f308f08fb
mndvns <mail@mndvns.com>
2015-07-29 19:37:12 -0600

    update root.js in ui

--- files/ui/src/root.js
+++ files/ui/src/root.js
@@ -1,17 +1,1 @@
-/**
- * Module dependencies
- */
-
-import Color from 'ui-kit/utils/color.js';
-import * as branding from 'ui-kit/utils/branding.js';
-
-exports = module.exports = branding;
-
-// colors
-
-var color = exports.color;
-
-for (var key in color)
-  color[key] = new Color(color[key]);
-
-export var color = color;
+module.exports = require('ui-kit/utils/root')();
